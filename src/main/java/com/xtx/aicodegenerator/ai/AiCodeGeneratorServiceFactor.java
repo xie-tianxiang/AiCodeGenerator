7a03ded66391e6fd1096f3f4c2c0361774be1561
xietianxiang <1228303666@qq.com>
2026-01-14 00:10:25 +0800

    feat：集成AI代码生成功能 1.接入DeepSeek API 2.实现代码生成逻辑与保存逻辑 3.提供外观类统一入口

---
+++ src/main/java/com/xtx/aicodegenerator/ai/AiCodeGeneratorServiceFactor.java
@@ -0,0 +1,19 @@
+package com.xtx.aicodegenerator.ai;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiCodeGeneratorServiceFactor {
+
+    @Resource
+    private ChatModel chatModel;
+
+    @Bean
+    public AiCodeGeneratorService aiCodeGeneratorService() {
+        return AiServices.create(AiCodeGeneratorService.class, chatModel);
+    }
+}
---
+++ src/test/java/com/xtx/aicodegenerator/ai/AiCodeGeneratorServiceTest.java
@@ -0,0 +1,27 @@
+package com.xtx.aicodegenerator.ai;
+
+import com.xtx.aicodegenerator.ai.model.HtmlCodeResult;
+import com.xtx.aicodegenerator.ai.model.MultiFileCodeResult;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做一个博客界面，不超过20行");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult result = aiCodeGeneratorService.generateMultiFileCode("做一个留言板，不超过50行");
+        Assertions.assertNotNull(result);
+    }
+}
